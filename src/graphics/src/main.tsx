2879c87f939a5ec08a8cee79263576f6b2701a76
SANDO Keishi <50182133+KeishiS@users.noreply.github.com>
2024-09-30 02:12:03 +0900

    一旦マージ (#2)

--- src/graphics/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import LiveIcon from './LiveIcon';
-
-createRoot(document.getElementById('container')!).render(
-    <>
-        <div>Hello World</div>
-        <LiveIcon />
-    </>
-);
