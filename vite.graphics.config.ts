0d89b6bd30693c00ca534c1c3faee3f86cc1163c
KeishiS <sando.keishi.sp@alumni.tsukuba.ac.jp>
2024-09-29 18:35:34 +0900

    updated

--- vite.graphics.config.ts
+++ vite.graphics.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'graphis',
+    outDir: 'graphics',
     rollupOptions: {
       input: {
         graphics: resolve(__dirname, 'src/graphics/graphics.html'),
