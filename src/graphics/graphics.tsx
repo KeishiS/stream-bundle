f1360edc856a3e3e4b2a517123613154d5ab9049
KeishiS <sando.keishi.sp@alumni.tsukuba.ac.jp>
2024-10-02 06:45:51 +0900

    updated

--- src/graphics/graphics.tsx
+++ src/graphics/graphics.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import LiveIcon from './components/LiveIcon';
+import InfoBox from './components/InfoBox';
 
 const app = (
   <>
     <LiveIcon />
+    <InfoBox />
   </>
 );
 
